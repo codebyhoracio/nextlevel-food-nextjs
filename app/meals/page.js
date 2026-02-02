fd89d142b8676da6fad5c5a97fe6200348a1e769
Horacio Guerra <achegideas@gmail.com>
2026-02-02 16:36:41 -0400

    feat: implement meals grid and item components, and update meals page layout with new styling.

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,20 +1,25 @@
 import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
 
 export default function MealsPage() {
   return (
     <>
-      <main>
-        <h1>Meals</h1>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
       </main>
-      <p>
-        <Link href="/meals/meal-1">Meal 1</Link>
-      </p>
-      <p>
-        <Link href="/meals/meal-2">Meal 2</Link>
-      </p>
-      <p>
-        <Link href="/meals/meal-3">Meal 3</Link>
-      </p>
     </>
   );
 }
